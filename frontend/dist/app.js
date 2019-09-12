f553da9488870918f2b6eebbf0bac3002ce1fe00
贾平 <ping.jia@zhangmen.com>
2019-09-12 16:20:27 +0800

    first commit

---
+++ frontend/dist/app.js
@@ -0,0 +1,23 @@
+
+
+var demo = new Vue({
+
+
+    el: '#main',
+
+    data: {
+        show_tooltip: false,
+        text_content: 'Edit me.'
+    },
+
+    // Functions we will be using.
+    methods: {
+        hideTooltip: function(){
+            // When a model is changed, the view will be automatically updated.
+            this.show_tooltip = false;
+        },
+        toggleTooltip: function(){
+            this.show_tooltip = !this.show_tooltip;
+        }
+    }
+    })
